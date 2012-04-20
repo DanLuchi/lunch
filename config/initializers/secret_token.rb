7f1a8cb0becb173ce255bfd6e761186e85514775
Dan Luchi <dan.luchi@bookrenter.com>
2012-04-19 21:32:41 -0700

    initial commit created with RailsApps Template: https://github.com/RailsApps/rails3-bootstrap-devise-cancan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunch::Application.config.secret_token = '27b7781d6c5428664152fee30bf04928eb41651d10c603980d20e61db6e48774b34005e02f2053a597bbfdc980e49e7ae34164410e600a1ce4a3614f3393f9de'
