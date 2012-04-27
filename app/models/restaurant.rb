e2177d856803db665cd46fb48bead6c7530d8673
Dan Luchi <dan.luchi@bookrenter.com>
2012-04-27 11:58:18 -0700

    add yelp_url to restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :name
+  has_many :user_restauants
+  has_many :users, :through => :user_restauants
+  has_many :lunch_restauants
+  has_many :lunches, :through => :lunch_restauants
+
+  attr_accessible :name, :yelp_url
 end
